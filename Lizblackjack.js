26f51b352df2595bcc8934c7af0310eb866c2671
Liz Swain <swainea@gmail.com>
2016-03-24 08:58:22 -0400

    added function for a random card

--- Lizblackjack.js
+++ Lizblackjack.js
@@ -1,15 +1,17 @@
 
 function runGame() {
 
+
     var display = document.querySelector('figure');
     var cards = ['2','3','4','5','6','7','8', '9', '10', 'J', 'Q', 'K', 'A'];
 
+    display.innerHTML = cards[drawCard()];
+    display.innerHTML = display.innerHTML + ' ' + cards[drawCard()];
+
     function checkResult(standing, hitting) {
         var hand = display.innerHTML.split(' ');
-        // console.log(hand);  // gives you an array of strings
-        var cardValue = 0; // start at 0 then will be the sum of the users cards as hit happens
+        var cardValue = 0;
 
-        // we want "cards" here to be the string from the array
         hand.forEach(function determineValue(card, i) {
             if (Number(card)) {
                 cardValue = cardValue + Number(card);
@@ -23,7 +25,7 @@ function runGame() {
               cardValue = cardValue + 11;
             }
 
-        }); console.log(cardValue);
+        });
 
             if (cardValue < 16 && standing) {
                 alert('Dealer wins.');
@@ -38,28 +40,22 @@ function runGame() {
                 alert('You Bust.');
             }
 
-      // display.innerHTML = '';
-      // card = (Math.floor(Math.random() * cards.length));
-      // display.innerHTML = cards[card];
     }
 
     document.getElementById('stand').addEventListener('click', function () {
       checkResult(true,false);
-      console.log("stand");
     });
 
-    card = (Math.floor(Math.random() * cards.length));
-    display.innerHTML = cards[card]; // this adds one card in runGame
-    card = (Math.floor(Math.random() * cards.length));
-    display.innerHTML = display.innerHTML + ' ' + cards[card]; // this takes the new random number to add a second card
 
-    console.log(display.innerHTML);
+    function drawCard(){
+       return (Math.floor(Math.random() * cards.length));
+    }
+    console.log(drawCard());
+
 
     document.getElementById('hit').addEventListener('click', function() {
-          var card = (Math.floor(Math.random() * cards.length));
-          display.innerHTML = display.innerHTML + " " + cards[card]; // fine up to here ?//
-          checkResult(false,true); // if its
-      // console.log("hit");
+          display.innerHTML = display.innerHTML + ' ' + cards[drawCard()];
+          checkResult(false,true);
     });
 
 }
